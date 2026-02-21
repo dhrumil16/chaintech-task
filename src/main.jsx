76268ac276437d05843c1335796c5bb80930468d
dhrumil16 <dhrumildk16127@gmail.com>
2026-02-21 18:08:37 +0530

    feat: complete implementation of all requested features including Platzi API, session timer, password toggle, and recent items

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext.jsx'
 import { CartProvider } from './context/CartContext.jsx'
 import { ToastProvider } from './context/ToastContext.jsx'
+import { RecentItemsProvider } from './context/RecentItemsContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -13,7 +14,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <ToastProvider>
         <AuthProvider>
           <CartProvider>
-            <App />
+            <RecentItemsProvider>
+              <App />
+            </RecentItemsProvider>
           </CartProvider>
         </AuthProvider>
       </ToastProvider>
